b8de6cca41c0a46c340a1c2048c1406a42d27dc5
GabriPires <gabriel.santos.pes@gmail.com>
2020-08-03 14:00:03 -0300

    Estilização desktop e separação do componente

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
-
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
 
@@ -29,26 +28,7 @@ const TeacherList: React.FC = () => {
       </PageHeader>
 
       <main>
-        <article className="teacher-item">
-          <header>
-            <img src="https://avatars3.githubusercontent.com/u/57969267?s=460&u=2ac71b8d5099df7a6058679c0f0515c460de0c1e&v=4" alt="Gabriel Pires"/>
-            <div>
-              <strong>Gabriel Pires</strong>
-              <span>História</span>
-            </div>
-          </header>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat aut pariatur, cum tempore explicabo vero doloremque, veritatis, atque provident unde nihil voluptates rerum quibusdam expedita ab possimus nostrum voluptatum maiores?</p>
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$ 50,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="Whatsapp"/>
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem />
       </main>
     </div>
   );
