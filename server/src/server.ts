1b4276e7d7943e7cb1004a93d75a86e3d7678b1c
GabriPires <gabriel.santos.pes@gmail.com>
2020-08-04 10:30:38 -0300

    Inicio do back do Proffy

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,21 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/users', (request, response) => {
-  const users = [
-    { name: 'Gabriel', age: 20 },
-    { name: 'Zeck', age: 18 }
-  ]
-  return response.json(users)
-});
-
-app.post('/users', (request, response) => {
-  console.log(request.body);
-
-  const users = [
-    { name: 'Gabriel', age: 20 },
-    { name: 'Zeck', age: 18 }
-  ]
-  return response.json(users)
+  return response.json({ message: "Hello stranger" })
 });
 
 app.listen(3333);
