5111bfe869fb314e9698a8f6231968816cc7147d
GabriPires <gabriel.santos.pes@gmail.com>
2020-08-04 10:16:49 -0300

    Rota post Express entendendo json

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,19 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/users', (request, response) => {
+  const users = [
+    { name: 'Gabriel', age: 20 },
+    { name: 'Zeck', age: 18 }
+  ]
+  return response.json(users)
+});
+
+app.post('/users', (request, response) => {
+  console.log(request.body);
+
   const users = [
     { name: 'Gabriel', age: 20 },
     { name: 'Zeck', age: 18 }
