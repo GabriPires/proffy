11ee5addadb62398f436be9e299811b6767b675c
GabriPires <gabriel.santos.pes@gmail.com>
2020-08-04 10:07:11 -0300

    Rota de usuários genérica

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.get('/users', (request, response) => {
+  const users = [
+    { name: 'Gabriel', age: 20 },
+    { name: 'Zeck', age: 18 }
+  ]
+  return response.json(users)
+});
+
 app.listen(3333);
