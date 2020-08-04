6f77ac5fe163d0fce92359c94d9d468fda5e78ed
GabriPires <gabriel.santos.pes@gmail.com>
2020-08-04 12:43:31 -0300

    Inicio da listagem de aulas FIltro de matéria

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -10,6 +10,27 @@ interface ScheduleItem {
 }
 
 export default class ClassesController {
+  async index(request: Request, response: Response) {
+    const filters = request.query;
+
+    const subject = filters.subject as string;
+    const week_day = filters.week_day as string;
+    const time = filters.time as string;
+
+    if(!filters.week_day || !filters.subject || !filters.time) {
+      return response.status(400).json({
+        error: 'Missing filter to search classes'
+      })
+    }
+
+    const timeInMinutes = convertHourToMinutes(time);
+
+    const classes = await db('classes')
+      .where('classes.subject', '=', subject)
+
+    return response.json(classes);
+  }
+
   async create(request: Request, response: Response) {
     const {
       name,
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,5 +7,6 @@ const routes = express.Router();
 const ClassesControllers = new ClassesController();
 
 routes.post('/classes', ClassesControllers.create);
+routes.get('/classes', ClassesControllers.index);
 
 export default routes;
