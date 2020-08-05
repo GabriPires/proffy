6268407d603fb42012c6eae220c374c6ad0390b6
GabriPires <gabriel.santos.pes@gmail.com>
2020-08-05 12:08:29 -0300

    Listagem e busca de professores

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,21 +4,35 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+};
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars3.githubusercontent.com/u/57969267?s=460&u=2ac71b8d5099df7a6058679c0f0515c460de0c1e&v=4" alt="Gabriel Pires"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Gabriel Pires</strong>
-          <span>História</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat aut pariatur, cum tempore explicabo vero doloremque, veritatis, atque provident unde nihil voluptates rerum quibusdam expedita ab possimus nostrum voluptatum maiores?</p>
+      <p>{teacher.bio}</p>
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 50,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
         <button type="button">
           <img src={whatsappIcon} alt="Whatsapp"/>
