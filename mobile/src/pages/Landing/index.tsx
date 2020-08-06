ea22d5e887b34c8b5f0943b27f384b6a632c176a
GabriPires <gabriel.santos.pes@gmail.com>
2020-08-06 11:49:35 -0300

    Estilização do component header

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { RectButton } from 'react-native-gesture-handler'
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
