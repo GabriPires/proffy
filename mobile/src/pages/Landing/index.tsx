b2e814aeb169d45a04343279b5bef1cf92d4cd06
GabriPires <gabriel.santos.pes@gmail.com>
2020-08-06 09:42:55 -0300

    Criação da landing page e estilização inicial

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text } from 'react-native';
 
-// import { Container } from './styles';
+import styles from './styles';
+
+import landingImage from '../../assets/images/landing.png';
 
 const Landing: React.FC = () => {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <Image source={landingImage} style={styles.banner} />
+
+      <Text style={styles.title}>
+        Seja bem-vindo, {'\n'}
+        <Text style={styles.titleBold}>
+          O que deseja fazer?
+        </Text>
+      </Text>
+    </View>
+  );
 }
 
 export default Landing;
