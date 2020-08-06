75532bc131d4faa1d04d156484919960a34afaef
GabriPires <gabriel.santos.pes@gmail.com>
2020-08-06 10:05:41 -0300

    Conteúdo da landing page

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 import landingImage from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 
 const Landing: React.FC = () => {
   return (
@@ -16,6 +18,18 @@ const Landing: React.FC = () => {
           O que deseja fazer?
         </Text>
       </Text>
+
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+          <Image source={studyIcon} />
+          <Text style={styles.buttonText}>Estudar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+          <Image source={giveClassesIcon} />
+          <Text style={styles.buttonText}>Dar aulas</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
