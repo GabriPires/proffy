4a9cef232cd937cb73f6a29ab9798a7aedee6cdc
GabriPires <gabriel.santos.pes@gmail.com>
2020-08-06 10:41:49 -0300

    Botão que leva pra tela dar aulas

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import styles from './styles';
 
@@ -9,6 +10,12 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
 const Landing: React.FC = () => {
+  const { navigate } = useNavigation();
+
+  function handleNavigateToGiveClassPage() {
+    navigate('GiveClasses');
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImage} style={styles.banner} />
@@ -26,7 +33,10 @@ const Landing: React.FC = () => {
           <Text style={styles.buttonText}>Estudar</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+        <TouchableOpacity
+          style={[styles.button, styles.buttonSecondary]}
+          onPress={handleNavigateToGiveClassPage}
+        >
           <Image source={giveClassesIcon} />
           <Text style={styles.buttonText}>Dar aulas</Text>
         </TouchableOpacity>
