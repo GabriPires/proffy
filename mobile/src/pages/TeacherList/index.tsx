ee3100c1bd1f3b56cb9c44d9c838909399948ee9
GabriPires <gabriel.santos.pes@gmail.com>
2020-08-07 11:52:56 -0300

    Favoritando e desfavoritando professor

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, ScrollView, Text, TextInput } from 'react-native';
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
@@ -13,27 +13,32 @@ import styles from './styles';
 
 const TeacherList: React.FC = () => {
   const [teachers, setTeachers] = useState([]);
-  const [favorites, setFavorites] = useState([]);
+  const [favorites, setFavorites] = useState<number[]>([]);
   const [isFiltersVisible, setIsFiltersVisible] = useState(false);
 
   const [subject, setSubject] = useState('');
   const [week_day, setWeekDay] = useState('');
   const [time, setTime] = useState('');
 
-  useEffect(() => {
+  function loadFavorites() {
     AsyncStorage.getItem('favorites').then(response => {
       if(response) {
-        setFavorites(JSON.parse(response));
+        const favoritedTeachers = JSON.parse(response);
+        const favoritedTeachersIds = favoritedTeachers.map((teacher: Teacher ) => {
+          return teacher.id;
+        })
+        setFavorites(favoritedTeachersIds);
       }
     })
-  }, [])
-
+  }
 
   function heandleToggleFiltersVisible() {
     setIsFiltersVisible(!isFiltersVisible);
   }
 
   async function handleFitlersSubmit() {
+    loadFavorites();
+
     const response = await api.get('/classes', {
       params: {
       subject,
@@ -106,7 +111,13 @@ const TeacherList: React.FC = () => {
         }}
       >
         { teachers.map((teacher: Teacher)=> {
-          return <TeacherItem key={teacher.id} teacher={teacher} />
+          return (
+            <TeacherItem
+              key={teacher.id}
+              teacher={teacher}
+              favorited={favorites.includes(teacher.id)}
+            />
+          );
         }) }
       </ScrollView>
     </View>
